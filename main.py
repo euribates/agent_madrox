8bbcc10cec2e4fbec13b63342e43f67dd39b1572
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2022-09-07 08:39:58 +0100

    Alpha version

--- main.py
+++ main.py
@@ -2,33 +2,174 @@
 # -*- coding: utf-8 -*-
 
 from datetime import date, datetime
+from contextlib import contextmanager
 
 from pony.orm import Database, set_sql_debug, db_session, select
 
 from models import open_source_database, open_target_database
 
 
+def red(msg):
+    """Devuelve el texto en color rojo (Para terminal/consola ANSI)."""
+    return f"\u001b[31m{msg}\u001b[0m"
 
-hoy = date.today()
-db = Database()
-print(db)
 
-db_source = open_source_database()
-db_target = open_target_database()
-with db_session():
-    for j in select(_ for _ in db_source.Jornada if _.fecha >= hoy):
-        id_sala = j.sala.id_sala
-        sala = db_target.Sala[id_sala]
-        if j.sesion:
-            sesion = db_target.Sesion.get(id_sesion=j.sesion.id_sesion)
-            print(j, sesion, sesion is None)
-        # print(j.id_jornada, j.descripcion) # , sala == j.sala)
-        # print(
-            # all([
-                # sala.id_sala == j.sala.id_sala,
-                # sala.descripcion == j.sala.descripcion,
-                # sala.url == j.sala.url,
-                # sala.rutaimagen == j.sala.rutaimagen,
-            # ])
-        # )
-        # print(j.sesion)
+def green(msg):
+    """Devuelve el texto en color verde (Para terminal/consola ANSI).
+    """
+    return f"\u001b[32m{msg}\u001b[0m"
+
+
+def yellow(msg):
+    """Devuelve el texto en color amarillo (Para terminal/consola ANSI).
+    """
+    return f"\u001b[33m{msg}\u001b[0m"
+
+
+ERROR = red("[✖]")
+OK = green("[✓]")
+SKIPPED = yellow("[⤼]")
+
+
+def migrar_sala(db_source, db_target, id_sala: int) -> int:
+    if id_sala:
+        source_item = db_source.Sala.get(id_sala=id_sala)
+        if source_item:
+            target_item = db_target.Sala.get(id_sala=id_sala)
+            if not target_item:  # Insert
+                values = source_item.to_dict(exclude="id_sala")
+                new_sesion = db_target.Sala(id_sala=id_sala, **values)
+                new_sesion.flush()
+                print(f"- Update Sala {OK}")
+                return 1
+            new_values = source_item.to_dict(exclude="id_sala")
+            old_values = target_item.to_dict(exclude="id_sala")
+            if new_values != old_values:  # Update
+                target_item.set(**new_values)
+                target_item.flush()
+                print(f"- Insert Sala {OK}")
+                return 1
+    print(f"- Skipped Sala {SKIPPED}")
+    return 0
+
+
+def migrar_organo(db_source, db_target, id_organo: str) -> int:
+    if id_organo:
+        source_item = db_source.Organo.get(id_organo=id_organo)
+        if source_item:
+            target_item = db_target.Organo.get(id_organo=id_organo)
+            if not target_item:  # Insert
+                values = source_item.to_dict(exclude="id_organo")
+                new_sesion = db_target.Organo(id_organo=id_organo, **values)
+                new_sesion.flush()
+                print(f"- Update Organo {OK}")
+                return 1
+            new_values = source_item.to_dict(exclude="id_organo")
+            old_values = target_item.to_dict(exclude="id_organo")
+            if new_values != old_values:  # Update
+                target_item.set(**new_values)
+                target_item.flush()
+                print(f"- Insert Organo {OK}")
+                return 1
+    print(f"- Skipped Organo {SKIPPED}")
+    return 0
+
+
+def migrar_sesion(db_source, db_target, id_sesion: str) -> int:
+    if id_sesion:
+        source_item = db_source.Sesion.get(id_sesion=id_sesion)
+        if source_item:
+            if source_item.id_organo:
+                migrar_organo(db_source, db_target, source_item.id_organo)
+            target_item = db_target.Sesion.get(id_sesion=id_sesion)
+            if not target_item:  # Insert
+                values = source_item.to_dict(exclude="id_sesion")
+                new_sesion = db_target.Sesion(id_sesion=id_sesion, **values)
+                new_sesion.flush()
+                print(f"- Update Sesion {OK}")
+                return 1
+            new_values = source_item.to_dict(exclude="id_sesion")
+            old_values = target_item.to_dict(exclude="id_sesion")
+            if new_values != old_values:  # Update
+                target_item.set(**new_values)
+                target_item.flush()
+                print(f"- Insert Sesion {OK}")
+                return 1
+    print(f"- Skipped Sesion {SKIPPED}")
+    return 0
+
+
+def migrar_sesion_datos(db_source, db_target, id_sesion: str) -> int:
+    if id_sesion:
+        source_item = db_source.SesionDatos.get(id_sesion=id_sesion)
+        if source_item:
+            target_item = db_target.SesionDatos.get(id_sesion=id_sesion)
+            if not target_item:  # Insert
+                values = source_item.to_dict(exclude="id_sesion")
+                new_sesion = db_target.SesionDatos(id_sesion=id_sesion, **values)
+                new_sesion.flush()
+                print(f"- Update SesionDatos {OK}")
+                return 1
+            new_values = source_item.to_dict(exclude="id_sesion")
+            old_values = target_item.to_dict(exclude="id_sesion")
+            if new_values != old_values:  # Update
+                target_item.set(**new_values)
+                target_item.flush()
+                print(f"- Insert SesionDatos {OK}")
+                return 1
+    print(f"- Skipped SesionDatos {SKIPPED}")
+    return 0
+
+
+def migrar_jornada(db_source, db_target, id_jornada):
+    if id_jornada:
+        source_item = db_source.Jornada.get(id_jornada=id_jornada)
+        if source_item.id_sala:
+            migrar_sala(db_source, db_target, source_item.id_sala)
+        if source_item.id_organo:
+            migrar_organo(db_source, db_target, source_item.id_organo)
+        if source_item.id_sesion:
+            migrar_sesion(db_source, db_target, source_item.id_sesion)
+            migrar_sesion_datos(db_source, db_target, source_item.id_sesion)
+        if source_item:
+            target_item = db_target.Jornada.get(id_jornada=id_jornada)
+            if not target_item:  # Insert
+                values = source_item.to_dict(exclude="id_jornada")
+                new_sesion = db_target.Jornada(id_jornada=id_jornada, **values)
+                new_sesion.flush()
+                print(f"- Update Jornada {OK}")
+                return 1
+            new_values = source_item.to_dict(exclude="id_jornada")
+            old_values = target_item.to_dict(exclude="id_jornada")
+            if new_values != old_values:  # Update
+                target_item.set(**new_values)
+                target_item.flush()
+                print(f"- Insert Jornada {OK}")
+                return 1
+    print(f"- Skipped Jornada {SKIPPED}")
+    return 0
+
+
+@contextmanager
+def frame_decoration(titulo, *args, **kwargs):
+    l = len(titulo)
+    print(f"--[ {titulo} ]{'-'* (66 - l)}")
+    try:
+        yield
+    finally:
+        print(f"{'-'*72}")
+
+
+def main():
+    hoy = date.today()
+    db_source = open_source_database()
+    db_target = open_target_database()
+    with db_session():
+        for jornada in select(_ for _ in db_source.Jornada if _.fecha >= hoy):
+            with frame_decoration(f"Jornada {jornada.id_jornada}"):
+                print(f"- {jornada.descripcion}")
+                migrar_jornada(db_source, db_target, jornada.id_jornada)
+
+
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -7,7 +7,6 @@ from pony.orm import (
     Optional,
     PrimaryKey,
     Required,
-    Set,
     set_sql_debug,
 )
 
@@ -25,7 +24,7 @@ def define_entities(db):
         legislatura = Required(int)
         alias = Required(str, max_len=32)
         nombre = Required(str, max_len=250)
-        direccion = Required(str, max_len=250)
+        direccion = Optional(str, max_len=250)
         constitucion = Required(date)
         disolucion = Optional(date)
         cp = Optional(str, max_len=5)
@@ -36,7 +35,7 @@ def define_entities(db):
         tipo = Required(int)
         id_usuario = Optional(str, max_len=8),
         ts_mod = Required(str, max_len=14),
-        tipo_comision = Required(int)
+        tipo_comision = Optional(int)
         codorg = Required(int)
         id_sala_habitual = Optional(int)
         id_tramite_organo = Optional(str, max_len=6),
@@ -45,7 +44,7 @@ def define_entities(db):
         descripcion = Optional(str, max_len=4000),
         id_letrado = Optional(str, max_len=5),
         nombre_completo = Optional(str, max_len=500)
-        jornadas = Set("Jornada")
+
 
     class Sesion(db.Entity):
         _table_ = ("AGORA", "SESION")
@@ -57,20 +56,20 @@ def define_entities(db):
         legislatura = Required(int)
         n_sesion = Required(int)
         tipo_sesion = Required(str, max_len=1)
-        descripcion = Required(str, max_len=512)
+        descripcion = Optional(str, max_len=512)
         id_sala = Required(int, default=0)
         n_asistentes = Required(int, default=0),
-        prensa = Required(int, default=0)
+        prensa = Required(str, max_len=1)
         ts_mod = Required(str, max_len=14)
         id_usuario = Required(str, max_len=8)
         n_dias = Required(int, default=1)
         migrable = Required(str, max_len=1)
-        jornadas = Set("Jornada")
+
 
     class SesionDatos(db.Entity):
         _table_ = ("AGORA", "SESION_DATOS")
 
-        id_sesion = PrimaryKey(int)
+        id_sesion = PrimaryKey(str, max_len=11)
         convocada = Required(str)
         f_convocada = Required(date)
         f_desconvocada = Optional(date)
@@ -82,6 +81,7 @@ def define_entities(db):
             py_check=lambda val: val in {'S', 'N'},
         )
 
+
     class Sala(db.Entity):
         _table_ = ("AGORA", "SALA")
 
@@ -89,7 +89,6 @@ def define_entities(db):
         descripcion = Required(str, max_len=512)
         url = Optional(str, max_len=255)
         rutaimagen = Optional(str, max_len=255)
-        jornadas = Set('Jornada')
 
         def __str__(self):
             if self.descripcion.lower().startswith('sala '):
@@ -97,18 +96,19 @@ def define_entities(db):
             else:
                 return f"Sala {self.descripcion} [{self.id_sala}]"
 
+
     class Jornada(db.Entity):
         _table_ = ("AGORA", "JORNADA")
 
         id_jornada = PrimaryKey(int)
         n_grabaciones = Required(int, default=0)
-        sesion = Optional(Sesion, column='ID_SESION')
-        organo = Optional(Organo, column='ID_ORGANO')
+        id_sesion = Optional(str, max_len=11)
+        id_organo = Optional(str, max_len=6)
         fecha = Required(date)
         hora = Required(str, max_len=5)
         descripcion = Required(str, max_len=512)
         texto = Optional(str, max_len=4000, default='')
-        sala = Optional(Sala, column='ID_SALA')
+        id_sala = Optional(int)
         canal = Required(int, default=0)
         hora_fin = Optional(str, max_len=5)
         id_sesion_seneca = Optional(int)
@@ -118,6 +118,18 @@ def define_entities(db):
         hora_reanudacion = Optional(str, max_len=5)
 
     db.generate_mapping()
+    return db
+
+
+def open_source_database():
+    db = Database()
+    db.bind(
+        provider='oracle',
+        dsn='oracle',
+        user='agora',
+        password='agora',
+    )
+    define_entities(db)
     return db
 
 
@@ -132,14 +144,3 @@ def open_target_database():
     define_entities(db)
     return db
 
-
-def open_source_database():
-    db = Database()
-    db.bind(
-        provider='oracle',
-        dsn='oracle',
-        user='agora',
-        password='agora',
-    )
-    define_entities(db)
-    return db
