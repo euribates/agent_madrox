86296bf838c22d5c895e157f0c75cdec26368120
Juan Ignacio Rodriguez de Leon <jileon@parcan.es>
2025-01-27 12:03:50 +0000

    Use pyenv now

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-cd $HOME/web/agent_madrox
-source $HOME/.virtualenvs/madrox/bin/activate
 python madrox.py migrate sesion --num-days 21
 python madrox.py migrate all --num-days 21
 
